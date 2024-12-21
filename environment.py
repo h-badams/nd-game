f64bae90aed4792614eb5764c909989da3c8bfb3
h-badams <henry.b.adams123@gmail.com>
2024-12-20 21:16:01 -0800

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,17 +1,17 @@
-import NDgame
-
 # environment for a two player game
 
 class Environment():
     def __init__(self, game):
         self.game = game
-    
+        
+    # runs a game between two agents, and returns the result - 1, 0, or 0.5 for the first player
     def run(self, agent_list, display_end=False, print_moves=False):
         turn = 0
         i = 0
         
         while True:
-            move = agent_list[i]((self.game.board, i+1, turn), (self.width, self.winLength))
+            move = agent_list[i]((self.game.board, i+1, turn, self.game.get_legal_moves()), (
+                self.game.width, self.game.dim))
             if self.game.is_legal(move):
                 self.game.play_move(move, i+1)
                 self.game.move_list.append(move)
@@ -19,9 +19,11 @@ class Environment():
                     print(move)
             else:
                 raise Exception("illegal move by player {i}")
+            #print("game board:", self.game)
+            #print("game result:", self.game.game_result(move, i + 1))
             if self.game.game_result(move, i+1) != -1:
                 if display_end:
-                    print(self.game)
+                    print("final game state: ", self.game)
                 return self.game.game_result(move, i+1)
             
             i += 1
