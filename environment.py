3abdb6be8902bb18795e64f9e8ff2c7a110f0a39
h-badams <henry.b.adams123@gmail.com>
2024-12-20 16:37:55 -0800

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,8 +1,10 @@
 import NDgame
 
+# environment for a two player game
+
 class Environment():
-    def __init__(self):
-        pass
+    def __init__(self, game):
+        self.game = game
     
     def run(self, agents):
         pass
