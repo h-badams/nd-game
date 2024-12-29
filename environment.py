6ad28086301cef887245d140f681429ff4d3b161
h-badams <henry.b.adams123@gmail.com>
2024-12-28 17:29:55 -0800

    Update environment.py

--- environment.py
+++ environment.py
@@ -5,12 +5,15 @@ class Environment():
         self.game = game
         
     # runs a game between two agents, and returns the result - 1, 0, or 0.5 for the first player
-    def run(self, agent_list, display_end=False, print_moves=False, return_move_num=False):
+    def run(self, agent_list, display_end=False, print_moves=False, return_move_num=False, is_rollout=False):
         
-        self.game.reset()
-        
-        turn = 0
-        i = 0
+        if not is_rollout:
+            self.game.reset()
+            turn = 0
+            i = 0
+        else:
+            turn = self.game.moves_played
+            i = turn % 2
         
         while True:
             move = agent_list[i]((self.game.board, i+1, turn, self.game.get_legal_moves()), (
@@ -23,15 +26,16 @@ class Environment():
             else:
                 raise Exception("illegal move by player {i}")
 
-            if self.game.game_result(move, i+1) != -1:
+            if self.game.game_result(move) != -1:
                 if display_end:
                     print("final game state: ", self.game)
                 if not return_move_num:
-                    return self.game.game_result(move, i+1)
+                    return self.game.game_result(move)
                 else:
-                    return self.game.game_result(move, i+1), self.game.moves_played
+                    return self.game.game_result(move), self.game.moves_played
             
             i += 1
             i %= 2
-            turn += 1
+            turn += 1          
+
             
