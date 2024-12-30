9b0ac5a8e8dbbc468b30ac6f01de16da8535ef1d
h-badams <henry.b.adams123@gmail.com>
2024-12-30 00:21:32 -0800

    Update agent-testing.py

--- agent-testing.py
+++ agent-testing.py
@@ -9,27 +9,32 @@ def agent_random(obs, config):
     moves = obs[3]
     return random.choice(moves)
 
-g = game.NDgame(2,3)
+g = game.NDgame(3,3)
 env = environment.Environment(g)
 a = agentMCTS.Agent()
 
-# Idea: can also change loop in env.run to just run width**dim times,
-# then call it a draw afterwards
-
-num_games = 10
+num_games = 20
 
 explore_step_size = 10
-max_explores = 300
+max_explores = 101
 explores_to_score = {}
 
-for i in range(1, max_explores, explore_step_size):
-    explores_to_score[i] = 0
-    for j in range(0, num_games, 1):
+for i in range(10, max_explores, explore_step_size):
+    a = agentMCTS.Agent(explore_const=i)
+    explores_to_score[i] = -10
+    for j in range(0, num_games, 2):
+        
         result = env.run([a.agent_mcts, agent_random], reset_before=True)
         explores_to_score[i] += result
         
         result = env.run([agent_random, a.agent_mcts], reset_before=True)
         explores_to_score[i] += 1 - result
+        
+    print(f"completed {i} explores")
     
 plt.bar(explores_to_score.keys(), explores_to_score.values())
+plt.xlabel('Explores')
+plt.ylabel('# Excess Wins (Out of 10)')
+plt.title('Excess Wins vs. Explores, 3x3x3 TicTacToe')
+
 plt.show()
